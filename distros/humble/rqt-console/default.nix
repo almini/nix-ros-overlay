2dbe159c5a1267c81e5157e406521aa4fcc17a91
Almin Iriskic <almin.iriskic@easelink.com>
2023-03-07 01:21:03 +0100

    rqt-console: add option for best effort qos

--- distros/humble/rqt-console/default.nix
+++ distros/humble/rqt-console/default.nix
@@ -2,16 +2,35 @@
 # Copyright 2022 Open Source Robotics Foundation
 # Distributed under the terms of the BSD license
 
-{ lib, buildRosPackage, fetchurl, ament-index-python, python-qt-binding, rcl-interfaces, rclpy, rqt-gui, rqt-gui-py, rqt-py-common }:
+{ lib, buildRosPackage, fetchgit, writeText, ament-index-python, python-qt-binding, rcl-interfaces, rclpy, rqt-gui, rqt-gui-py, rqt-py-common, qos-best-effort ? false }:
 buildRosPackage {
   pname = "ros-humble-rqt-console";
-  version = "2.0.2-r3";
+  version = "2.1.1";
 
-  src = fetchurl {
-    url = "https://github.com/ros2-gbp/rqt_console-release/archive/release/humble/rqt_console/2.0.2-3.tar.gz";
-    name = "2.0.2-3.tar.gz";
-    sha256 = "06759f530a897cb663e7d7648843466bfb775dc8bdd2f86adf727c1b46b6c760";
+  src = fetchgit {
+    url = "https://github.com/ros-visualization/rqt_console.git";
+    rev = "7b4797c29746ea1f39b05beac63d836a5ded8cd3";
+    hash = "sha256-afdhFwll4Y44GpmZIXLZmYztFt8RnvvVJFPRCxKGcGM=";
   };
+
+  patchFlags = "-p1 -l";
+  patches = if qos-best-effort then [
+    (writeText "use_best_effort.patch" ''
+      diff --git a/src/rqt_console/console.py b/src/rqt_console/console.py
+      index 04177a2..e285f2d 100644
+      --- a/src/rqt_console/console.py
+      +++ b/src/rqt_console/console.py
+      @@ -80,7 +80,7 @@ class Console(Plugin):
+              self._timer.timeout.connect(self.insert_messages)
+              self._timer.start(100)
+              self._qos = QoSProfile(
+      -            reliability=QoSReliabilityPolicy.RELIABLE,
+      +            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                  durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+                  history=QoSHistoryPolicy.SYSTEM_DEFAULT,
+                  liveliness=QoSLivelinessPolicy.AUTOMATIC,
+    '')
+  ] else [];
 
   buildType = "ament_python";
   propagatedBuildInputs = [ ament-index-python python-qt-binding rcl-interfaces rclpy rqt-gui rqt-gui-py rqt-py-common ];
